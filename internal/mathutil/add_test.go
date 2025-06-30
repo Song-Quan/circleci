99cb094bd44cda93d1bc3aba72e77dbbb4a5462d
Song-Quan <heangzhan.haz@antgroup.com>
2025-06-30 18:59:55 +0800

    add test

--- internal/mathutil/add_test.go
+++ internal/mathutil/add_test.go
@@ -9,3 +9,11 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Add(2, 3) = %d; want %d", result, expected)
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := Sub(2, 3)
+	expected := -1
+	if result != expected {
+		t.Errorf("Sub(2, 3) = %d; want %d", result, expected)
+	}
+}
